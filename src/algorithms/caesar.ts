3cdd5410869d1505130bcd0305052f2814e81379
sasuketiesma <sasuke.tiesma@gmail.com>
2025-08-05 13:54:59 -0400

    Replaced implementation with a slightly more robust version

--- src/algorithms/caesar.ts
+++ src/algorithms/caesar.ts
@@ -1,50 +1,24 @@
-import { isSpecialChar } from "../utils/modifyInput"
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-const encryptCaesar = ( message: string, shift: number ) => {
-    if (shift == 0) return message;
+const encryptCaesar = (message: string, shift: number, ignoreCase: boolean) => {
+    // const alphabetLower = alphabet.toLowerCase();
+    // const alphabetUpper = alphabet.toUpperCase();
+    let output = "";
+    const leastPositiveResidue = ((shift % 26) + 26) % 26;
 
-    shift = getLeastPositiveResidueMod26(shift);
-
-    let ciphertext = "";
-    for (let i = 0; i < message.length; i++) {
-        if (isSpecialChar(message[i])) {
-            ciphertext += message[i];
+    for (let i = 0, charNum: number, shiftedCharNum: number; i < message.length; i++) {
+        if (!alphabet.includes(message[i])) {
+            output += message[i];
             continue;
         }
-        ciphertext += shiftChar(message[i], shift);
+
+        charNum = alphabet.indexOf(message[i]);
+        shiftedCharNum = (charNum + leastPositiveResidue) % 26;
+
+        output += alphabet[shiftedCharNum];
     }
 
-    return ciphertext;
+    return output;
 }
-
-//helper functions
-const getLeastPositiveResidueMod26 = ( input: number ) => {
-    return ((input % 26) + 26) % 26;
-}
-
-//charNumber: a=0, b=1, ... , z=26
-const getCharNumber = ( charCode: number ) => {
-    const charCodeUppercaseA = 65;
-
-    return charCode - charCodeUppercaseA;
-}
-
-const getCharFromNumber = ( charNumber: number ) => {
-    const charCodeUppercaseA = 65;
-
-    return String.fromCharCode( charNumber + charCodeUppercaseA );
-}
-
-
-const shiftChar = ( char: string, shift: number ) => {
-    const alphabetSize = 26;
-    const charCode = char.charCodeAt(0);
-
-    const charNumber = getCharNumber(charCode);
-    const encryptedCharNumber = (charNumber + shift) % alphabetSize;
-
-    return getCharFromNumber(encryptedCharNumber);
-}   
-
 
 export default encryptCaesar;
--- src/routes/Caesar.tsx
+++ src/routes/Caesar.tsx
@@ -1,31 +1,27 @@
 import { useEffect, useState } from "react";
 import encryptCaesar from "../algorithms/caesar";
-import { filterText } from "../utils/modifyInput";
-
+import './Caesar.css';
 
 const Caesar = () => {
     const [ plaintext, setPlaintext ] = useState("");
     const [ shift, setShift ] = useState(0);
     const [ output, setOutput ] = useState("");
+    const [ ignoreCase, setIgnoreCase ] = useState(false);
 
     useEffect(() => {
-        const ciphertext =  encryptCaesar(plaintext, shift);
+        const ciphertext =  encryptCaesar(plaintext, shift, true);
         setOutput(ciphertext);
     }, [ plaintext, shift ]);
-
-
-    const onTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const filteredText = filterText(event.target.value).toUpperCase();
-        setPlaintext(filteredText);
-    }
 
     return (
         <div>
             <h1>Caesar Cipher</h1>
-            <input value={plaintext} placeholder="plaintext" onChange={(event) => {onTextChange(event)}}></input>
+            <input value={plaintext} placeholder="plaintext" onChange={(event) => {setPlaintext(event.target.value)}}></input>
             <p>shift: {shift}</p>
+            <p>ignore case: {String(ignoreCase)}</p>
             <button onClick={() => {setShift(shift + 1)}}>+</button>
             <button onClick={() => {setShift(shift - 1)}}>-</button>
+            <button onClick={() => {setIgnoreCase(!ignoreCase)}} >case</button>
             <p>{output}</p>
         </div>
     );
